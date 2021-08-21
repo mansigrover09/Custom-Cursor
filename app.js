fd1700bd4a6464fb12688dbfbc1034fb09d35246
Mansi <mansigrover2001@gmail.com>
2021-08-21 15:08:56 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const cursor = document.querySelector('.cursor');
+
+window.addEventListener('mousemove' , (e)=> {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+    cursor.setAttribute('data-fromTop', (cursor.offsetTop - scrollY))
+})
+window.addEventListener('scroll',()=> {
+    const fromTop = parseInt(cursor.getAttribute('data-fromTop'));
+    cursor.style.top = scrollY + fromTop + 'px';
+    console.log(scrollY);
+})
+window.addEventListener('click', ()=>{
+    cursor.classList.add('click')
+})
+    
